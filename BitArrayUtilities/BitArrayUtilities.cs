70c8d2c0ac22475a93634749c7e0a1a80becb1fc
Mikkel Nygaard <mnygaa15@student.aau.dk>
2016-04-27 15:23:31 +0200

    Ini Commit

---
+++ BitArrayUtilities/BitArrayUtilities.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Diagnostics.Contracts;
+
+namespace BitArrayUtilities {
+    public static class BitArrayUtilities {
+        /// <summary>
+        /// Change the endian encoding for the BitArray.
+        /// </summary>
+        /// <param name="bitArray"></param>
+        /// <returns>Returns BitArray with changed endian encoding.</returns>
+        public static BitArray ReverseEndianOnBitArray(BitArray bitArray) {
+            Contract.Requires(bitArray.Count % 8 == 0);
+            for (int i = 0; i < bitArray.Count / 8; i++) {
+                for (int k = i * 8, j = (1 + i) * 8 - 1, count = 0; count < 4; ++k, --j, count++) {
+                    bool temp = bitArray[k];
+                    bitArray[k] = bitArray[j];
+                    bitArray[j] = temp;
+                }
+            }
+
+            return bitArray;
+        }
+
+        /// <summary>
+        /// Reverse the entire BitArray
+        /// </summary>
+        /// <param name="bitArray"></param>
+        /// <returns>Returns BitArray with the bits in the reverse order.</returns>
+        public static BitArray ReverseBitArray(BitArray bitArray) {
+            Contract.Requires(bitArray.Count % 2 == 0);
+            for (int i = 0, j = bitArray.Count - 1; i < bitArray.Count / 2; i++, j--) {
+                bool temp = bitArray[i];
+                bitArray[i] = bitArray[j];
+                bitArray[j] = temp;
+            }
+
+            return bitArray;
+        }
+        
+        /// <summary>
+        /// Compare two BitArrays.
+        /// </summary>
+        /// <param name="ba1"></param>
+        /// <param name="ba2"></param>
+        /// <returns>Returns true if equals.</returns>
+        public static bool CompareBitArray(BitArray ba1, BitArray ba2) {
+            if (ba1.Length != ba2.Length) {
+                return false;
+            }
+
+            for (int i = 0; i < ba1.Length; i++) {
+                if (ba1[i] != ba2[i]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
